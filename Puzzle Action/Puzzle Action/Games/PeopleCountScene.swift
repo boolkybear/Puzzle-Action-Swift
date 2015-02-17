be98b0de3d5a102ffd0090df510ebe62e79147d4
Boolky Bear <boolkybear@gmail.com>
2015-02-17 16:11:38 +0100

    Create project, remove unnecessary files

---
+++ Puzzle Action/Puzzle Action/Games/PeopleCountScene.swift
@@ -0,0 +1,13 @@
+//
+//  PeopleCountScene.swift
+//  Puzzle Action
+//
+//  Created by Boolky Bear on 17/2/15.
+//  Copyright (c) 2015 ByBDesigns. All rights reserved.
+//
+
+import UIKit
+
+class PeopleCountScene: SKScene {
+   
+}
